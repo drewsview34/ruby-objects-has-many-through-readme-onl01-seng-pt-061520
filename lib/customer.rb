6ed79ff8f21d0071df56ff42613f7bc5871066e8
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-24 23:49:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,15 +13,17 @@ class Customer
     @@all
   end
   
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
- 
   def meals
     Meal.all.select do |meal|
       meal.customer == self
     end
   end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+
   
   def waiters
     meals.map do |meal|
